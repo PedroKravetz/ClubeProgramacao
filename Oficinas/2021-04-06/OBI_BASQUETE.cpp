d70fda48c3c7c1e84a889e08625e3346bb91866a
PedroKravetz <p.v.kravetz@gmail.com>
2023-01-22 08:43:05 -0300

    Exercícios do dia 6 de abril de 2021 adicionados

---
+++ Oficinas/2021-04-06/OBI_BASQUETE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int distancia;
+    scanf("%d", &distancia);
+    if (distancia < 81)
+        printf("%d\n", 1);
+    else if (distancia < 1401)
+        printf("%d\n", 2);
+    else
+        printf("%d\n", 3);
+}
